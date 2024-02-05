88fb4eef9ab95e96b7744fcedefc574cedcbab39
Aleksei Sivodedov <sivoded@gmail.com>
2024-02-05 12:01:21 +0400

    feat: add key and hmac generator

---
+++ src/ConsoleGame/Program.cs
@@ -0,0 +1,6 @@
+﻿using ConsoleGame.Services;
+
+var key = CryptoService.GenerateKey();
+
+Console.WriteLine("KEY: " + key);
+Console.WriteLine("HMAC: " + CryptoService.GenerateHMAC("stone", key));
---
+++ src/ConsoleGame/Services/CryptoService.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ConsoleGame.Services
+{
+    internal class CryptoService
+    {
+        public static string GenerateKey()
+        {
+            const byte bufferSize = 32;
+            using (var generator = RandomNumberGenerator.Create())
+            {
+                var buffer = new byte[bufferSize];
+                generator.GetBytes(buffer);
+                var key = Convert.ToHexString(buffer);
+                return key;
+            }
+        }
+
+        public static string GenerateHMAC(string text, string key)
+        {
+            key = key ?? "";
+
+            using (var hmacsha256 = new HMACSHA256(Encoding.UTF8.GetBytes(key)))
+            {
+                var hash = hmacsha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+                return Convert.ToHexString(hash);
+            }
+
+        }
+    }
+}
